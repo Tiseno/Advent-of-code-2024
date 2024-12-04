e436ee85ddd38e5ea30435057e8286742a6452a9
Tiseno <math.lindgren@gmail.com>
2024-12-04 07:26:21 +0100

    Day 4 part 2

--- day4/main.hs
+++ day4/main.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wall #-}
 import qualified Data.List       as List
 import           Text.Regex.TDFA
 
@@ -22,8 +21,28 @@ part1 input =
   + count "SAMX" (diagonalTranspose input)
   + count "SAMX" (diagonalTranspose2 input)
 
+isMas :: String -> Bool
+isMas a = a == "MAS" || a == "SAM"
+
+beginsWithXmas :: [(Char, Char, Char)] -> Bool
+beginsWithXmas ((tl,_,bl):(_,m,_):(tr,_,br):_) = isMas [tl,m,br] && isMas [bl,m,tr]
+beginsWithXmas _ = False
+
+countXmas :: [(Char, Char, Char)] -> Int
+countXmas [] = 0
+countXmas xs = (if beginsWithXmas xs then 1 else 0) + countXmas (tail xs)
+
+zipAll :: [String] -> [[(Char,Char,Char)]]
+zipAll (x:y:z:input) = zip3 x y z : zipAll (y:z:input)
+zipAll _             = []
+
+part2 :: [String] -> Int
+part2 input = sum (countXmas <$> zipAll input)
+
 main :: IO ()
 main = do
   input <- lines <$> readFile "input.txt"
   putStrLn "Part 1"
   print $ part1 input
+  putStrLn "Part 2"
+  print $ part2 input
