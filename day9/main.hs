55acffcecac1d5c436f75d92a73717353e3329f2
Tiseno <math.lindgren@gmail.com>
2024-12-09 18:29:13 +0100

    Day 9 part 2

--- day9/main.hs
+++ day9/main.hs
@@ -33,8 +33,85 @@ part1 (arr :: Array.Array Int Int) = go (0, 0) 0 0 (length arr - 1)
           then go (checksumIndex, files - emptyBlock) (position + emptyBlock) (leftIndex + 1) newRightIndex
           else fillFromBack (0, 0) (emptyBlock - files) (position + files) leftIndex newRightIndex
 
+type FileSystem = Array.Array Int Int
+
+parseInput2 :: String -> FileSystem
+parseInput2 input =
+  let sectors :: [Int] = fmap (\c -> read [c]) $ head $ lines input
+      fsSize = sum sectors
+      emptyFs = Array.listArray (0 :: Int, fsSize - 1) $ repeat (negate (1 :: Int))
+      indexedSectors = zip [(0 :: Int)..] sectors
+   in snd $ foldl foldSector (0, emptyFs) indexedSectors
+    where
+      foldSector (fsPosition :: Int, fs :: FileSystem) (index :: Int, size :: Int) | odd index = (fsPosition + size, fs)
+      foldSector (fsPosition, fs) (index, size) =
+        let fileId :: Int = fromIntegral $ index `div` 2
+         in (fsPosition + size, fs Array.// fmap (, fileId) [fsPosition..(fsPosition + size - 1)])
+
+findFile :: (Int, FileSystem) -> Maybe (Int, Int)
+findFile (index, _) | index < 0 = Nothing
+findFile (index, fs) | (fs Array.! index) == -1 = findFile (index - 1, fs)
+findFile (end, fs) = Just (findFileStart end, end)
+  where
+    fileId = fs Array.! end
+    findFileStart i =
+      if i <= 0 || (fs Array.! (i - 1)) /= fileId
+         then i
+         else findFileStart (i - 1)
+
+firstEmptySpaceOfSize :: Int -> FileSystem -> Int -> Maybe (Int, Int)
+firstEmptySpaceOfSize size fs beforeIndex = firstEmptySpaceOfSizeFrom 0
+  where
+    firstEmptySpaceOfSizeFrom from = case findSpaceSpan from of
+        Nothing -> Nothing
+        Just spaceSpan@(spaceStart, spaceEnd) ->
+          if spaceEnd - spaceStart + 1 >= size
+             then Just spaceSpan
+             else firstEmptySpaceOfSizeFrom (spaceEnd + 1)
+    findSpaceEnd i | (i + 1) >= beforeIndex = i
+    findSpaceEnd i | fs Array.! (i + 1) /= -1 = i
+    findSpaceEnd i = findSpaceEnd (i + 1)
+    findSpaceSpan i | i >= beforeIndex = Nothing
+    findSpaceSpan i | fs Array.! i /= -1 = findSpaceSpan (i + 1)
+    findSpaceSpan spaceStart = Just (spaceStart, findSpaceEnd spaceStart)
+
+swapData :: FileSystem -> Int -> Int -> Int -> FileSystem
+swapData fs len a b =
+  let aElem = fs Array.! a
+      bElem = fs Array.! b
+      newAData = [(i, bElem) | i <- [a..(a + len - 1)]]
+      newBData = [(i, aElem) | i <- [b..(b + len - 1)]]
+   in aElem `seq` bElem `seq` fs Array.// newAData Array.// newBData
+
+moveFileFromEndToFront :: (Int, FileSystem) -> (Int, FileSystem)
+moveFileFromEndToFront (index, fs) =
+  case findFile (index, fs) of
+    Nothing -> (-1, fs)
+    Just (fileStart, fileEnd) ->
+      let fileLength = (fileEnd - fileStart) + 1
+       in case firstEmptySpaceOfSize fileLength fs fileStart of
+         Nothing -> (fileStart - 1, fs)
+         Just (emptySpaceStart, _) -> (fileStart - 1, swapData fs fileLength fileStart emptySpaceStart)
+
+moveAllFiles :: (Int, FileSystem) -> (Int, FileSystem)
+moveAllFiles (index, fs) = until (\(i, _) -> i < 0) moveFileFromEndToFront (index, fs)
+
+calculateCheckSum :: FileSystem -> Int
+calculateCheckSum fs = calculateCheckSumIt 0 0
+  where
+    (_, end) = Array.bounds fs
+    calculateCheckSumIt acc index | index == end = acc
+    calculateCheckSumIt acc index = calculateCheckSumIt (acc + checkFunction (index, fs Array.! index)) (index + 1)
+    checkFunction (_, -1)         = 0
+    checkFunction (index, fileId) = fromIntegral index * fromIntegral fileId
+
+part2 :: FileSystem -> Int
+part2 (fs :: FileSystem) = calculateCheckSum $ snd $ moveAllFiles (snd $ Array.bounds fs, fs)
+
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "input.txt"
+  input <- readFile "input.txt"
   putStrLn "Part 1"
-  print $ part1 input
+  print $ part1 $ parseInput input
+  putStrLn "Part 2"
+  print $ part2 $ parseInput2 input
