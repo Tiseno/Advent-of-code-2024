6dcf571afbf1b07b181208fdc9e61d9e86efaffb
Tiseno <math.lindgren@gmail.com>
2024-12-11 08:24:11 +0100

    Day 11 part 2

--- day11/main.hs
+++ day11/main.hs
@@ -1,4 +1,7 @@
-{-# OPTIONS_GHC -Wall #-}
+import qualified Data.List.Extra as List
+import qualified Data.Map        as Map
+import qualified Data.Ord        as Ord
+
 parseInput :: String -> [Int]
 parseInput = fmap read . words . head . lines
 
@@ -10,11 +13,20 @@ transform stone | even $ length $ show stone =
    in [read $ take splitLength stoneString, read $ drop splitLength stoneString]
 transform stone = [stone * 2024]
 
-part1 :: [Int] -> Int
-part1 (input :: [Int]) = length (iterate (concatMap transform) input !! 25)
+transformCounted :: Map.Map Int Int -> Map.Map Int Int
+transformCounted stonesWithCount =
+  let newStonesWithCounts = concatMap (\(stone, count) -> (,count) <$> transform stone) (Map.assocs stonesWithCount)
+      grouped = List.groupSortBy (Ord.comparing fst) newStonesWithCounts
+      sumCounts = foldr (\(stone, count) (_, s) -> (stone, s + count)) (0, 0)
+   in Map.fromAscList $ fmap sumCounts grouped
+
+solve :: Int -> [Int] -> Int
+solve i input = foldr (\(_, c) s -> s + c) 0 $ Map.assocs $ iterate transformCounted (Map.fromAscList (fmap (,1) input)) !! i
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
   putStrLn "Part 1"
-  print $ part1 input
+  print $ solve 25 input
+  putStrLn "Part 2"
+  print $ solve 75 input
